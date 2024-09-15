647fc930eb9f2c575f2e57d74ecaf7af42e32200
Jazper <jazpersaldana@yahoo.com>
2024-09-15 15:54:14 -0500

    add Home Page & start NavBar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Link, Switch, Route } from "react-router-dom";
+
+import Home from "./Home"
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <>
+    <header>
+      <h1>NavBar</h1>
+    </header>
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <h1>Phase 4 Project</h1>
+        </Route>
+        <Route exact path="/Home">
+          <Home />
+        </Route>
+      </Switch>
+    </main>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
+function Home() {
+    return <h1>Home Page</h1>
+}
+
+export default Home
