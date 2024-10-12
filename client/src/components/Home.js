026b3a4ba31fc6b483f640a525f8ea3060a38668
Jazper <jazpersaldana@yahoo.com>
2024-10-12 14:03:26 -0500

    clean index.css and add content to Home Page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,12 @@ import React, { useEffect, useState } from "react";
 function Home() {
     return (
     <div className="home-container">
-        <div className="home-page">Home Page</div>
+        <div className="home-header">
+            Welcome to Percussion Playground
+        </div>
+        <div className="home-content">
+            A place for percussionists to buy, sell, trade, and share their love of instruments
+        </div>
     </div>
     )
 }
